fd00d97d99309ba07e0047744fe004e0a3980798
GiovannyGil <arvey950@gmail.com>
2024-07-24 16:44:07 -0500

    avance en kotlin (primer practica -> 'hola mundo')

---
+++ Kotlin/ConceptosBasicos/Arrays.kt
@@ -0,0 +1,26 @@
+/**
+    ARRAY
+
+    son una coleccion de elementos del MISMO TIPO que no se puede cambiar desde su creacion.
+    los arrays tienen una longitud fija
+    el acceso a los elementos es mediante el indice/index 
+ */
+
+ // Crear un array de enteros
+val numbers: Array<Int> = arrayOf(1, 2, 3, 4, 5)
+println(numbers.joinToString()) // Imprime: 1, 2, 3, 4, 5
+
+// Acceder a un elemento
+println(numbers[2]) // Imprime: 3
+
+// Modificar un elemento
+numbers[2] = 10
+println(numbers.joinToString()) // Imprime: 1, 2, 10, 4, 5
+
+// Crear un array de cadenas
+val fruits: Array<String> = arrayOf("Apple", "Banana", "Cherry")
+println(fruits.joinToString()) // Imprime: Apple, Banana, Cherry
+
+// Array con tamaño fijo y valores iniciales
+val zeros = Array(5) { 0 } // Crea un array de 5 elementos, todos inicializados a 0
+println(zeros.joinToString()) // Imprime: 0, 0, 0, 0, 0
---
+++ Kotlin/ConceptosBasicos/Listas.kt
@@ -0,0 +1,24 @@
+/**
+    LISTAS
+
+    (List) las listas son inmutables -> no se pueden modificar despues de su creacion/instanciacion 
+    MutableList son un tipo de listas mutables, que se peuden modificar luego de su creacion, NO tienen una longitud definidad, pueden crecer y disminuir
+ */
+
+//  LIST
+val fruits: List<String> = listOf("Apple", "Banana", "Cherry")
+println(fruits) // Imprime: [Apple, Banana, Cherry]
+println(fruits[1]) // Imprime: Banana
+
+// MutableList
+val fruits: MutableList<String> = mutableListOf("Apple", "Banana", "Cherry")
+println(fruits) // Imprime: [Apple, Banana, Cherry]
+
+fruits.add("Date")
+println(fruits) // Imprime: [Apple, Banana, Cherry, Date]
+
+fruits.remove("Banana")
+println(fruits) // Imprime: [Apple, Cherry, Date]
+
+fruits[1] = "Blueberry"
+println(fruits) // Imprime: [Apple, Blueberry, Date]
