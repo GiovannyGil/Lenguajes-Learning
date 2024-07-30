d237c676c9a782690fa92d14b41419cfbe35d56d
GiovannyGil <arvey950@gmail.com>
2024-07-30 16:37:30 -0500

    proyectos de practica de JS faltantes, avances en el curso de kotlin y inicios de la practica de diseño

---
+++ Kotlin/Practicas/PracticaDiseño/app/src/main/java/com/example/practicadiseo/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.example.practicadiseo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+
+val CustomPrimary = Color(0xFF0073EE)
+val CustomPrimaryVariant = Color(0xFF3700B3)
+val CustomSecondary = Color(0xFF03DAC6)
+val CustomPrimaryContainer = Color(0xFFD0BCFF)
+val CustomTitleContent = Color(0xFF000000)
