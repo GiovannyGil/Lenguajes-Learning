66fcd18f077202164c6bfd612b6bcb8234dac741
GiovannyGil <arvey950@gmail.com>
2024-08-08 08:15:26 -0500

    eliminar proyecto idiomas y agregar proyecto translate (lo mismo-diferente nombre)

---
+++ Kotlin/Practicas/TranslateApp/app/src/main/java/com/example/translateapp/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.translateapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.translateapp.languages.LanguagesView
+import com.example.translateapp.translator.TranslateView
+import com.example.translateapp.translator.TranslateViewModel
+import com.example.translateapp.ui.theme.TranslateAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val viewModel : TranslateViewModel by viewModels()
+        setContent {
+            TranslateAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TranslateView(viewModel)
+                   // LanguagesView()
+                   // MyView()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyView() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(id = R.string.title), fontWeight = FontWeight.Bold)
+        Text(text = stringResource(id = R.string.subtitle))
+    }
+}
---
+++ Kotlin/Practicas/TranslateApp/app/src/main/java/com/example/translateapp/languages/getStrings.kt
@@ -0,0 +1,21 @@
+package com.example.translateapp.languages
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun getStrings(): List<Map<String, String>> {
+    val en = mapOf(
+        "title" to "Hello World",
+        "subtitle" to "The World is yours"
+    )
+
+    val es = mapOf(
+        "title" to "Hola Mundo",
+        "subtitle" to "El Mundo es tuyo"
+    )
+
+    return listOf(
+        en,
+        es
+    )
+}
