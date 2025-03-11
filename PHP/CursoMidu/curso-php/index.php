c6c2637f549b679f64a0cc86865533efc3df0b62
Giovanny <102708414+GiovannyGil@users.noreply.github.com>
2025-03-10 21:01:36 -0500

    primer paso en PHP

---
+++ PHP/CursoMidu/curso-php/index.php
@@ -0,0 +1,41 @@
+<?php
+
+// crear variables
+/**
+ * estan son las variables que se crearon
+ * -> simbolo de dollar al inicio "$"
+ * -> nombre de la variable
+ * -> signo de igual "="
+ * -> valor de la variable
+ * 
+ * -> tipos (tipado DEBIL, tipado DINAMICO, tipado GRADUAL)
+ *      -> string -> texto
+ *      -> int -> numero entero
+ *      -> float -> numero decimal
+ *      -> boolean -> verdadero o falso
+ * 
+ */
+$name = "Giovanny";
+$age = 25;
+$height = 1.79;
+$country = "Colombia";
+$City = "Medellín";
+$job = "Software Developer";
+
+// imprimir variables -> en pantalla
+echo "Mi nombre es $name, tengo $age años, mido $height m, soy de $country, vivo en $City y soy $job";
+?>
+
+<style>
+    :root {
+        color-scheme: light dark;
+    }
+
+    body {
+        display: grid;
+        place-items: center;
+        font-size: 25px;
+        font-family: Arial, sans-serif;
+        color: #f1f1f1;
+    }
+</style>
