0f9fffd3c77b0f5786672640790d08ec284d13e7
Giovanny <102708414+GiovannyGil@users.noreply.github.com>
2026-04-15 14:12:32 -0500

    eliminar app

---
+++ Mobile/React-Native/FitnessApp/Mobile/android/app/build/intermediates/cxx/Debug/1z711g4k/logs/arm64-v8a/build_command_targets.bat
@@ -0,0 +1,6 @@
+@echo off
+"C:\\Users\\giogi\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Codes\\Lenguajes-Learning\\Mobile\\React-Native\\FitnessApp\\Mobile\\android\\app\\.cxx\\Debug\\1z711g4k\\arm64-v8a" ^
+  appmodules ^
+  react_codegen_safeareacontext
---
+++ Mobile/React-Native/FitnessApp/Mobile/android/app/build/intermediates/cxx/Debug/4s2e3m4v/logs/arm64-v8a/build_command_targets.bat
@@ -0,0 +1,6 @@
+@echo off
+"C:\\Users\\giogi\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Codes\\Lenguajes-Learning\\Mobile\\React-Native\\FitnessApp\\Mobile\\android\\app\\.cxx\\Debug\\4s2e3m4v\\arm64-v8a" ^
+  appmodules ^
+  react_codegen_safeareacontext
